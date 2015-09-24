61b7e1f6c55c12125a99f901ef3989c128cc6be1
aboynejames <james@aboynejames.co.uk>
2015-09-24 15:24:44 +0100

    accounting class added

---
+++ accounting/src/accounting.js
@@ -0,0 +1,16 @@
+/**
+* Dsensor - Mapping Protocol Accounting
+*
+*  Make blockchain entries to secure mppm scoring, consenus, resolution status
+*  
+* @class mAccounting
+*
+* @package    dsensor.og  open source project
+* @copyright  Copyright (c) 2015 James Littlejohn
+* @license    http://ww
+* @version    $Id$
+*/
+var mAccounting = function() {
+
+	
+};
